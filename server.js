5245a60e53adc43ae2f5004acfb0478c1624b8eb
Asli <aslinurkeklikci@yahoo.com>
2021-01-15 20:01:03 -0500

    added prompt user function

--- server.js
+++ server.js
@@ -27,8 +27,62 @@ function start() {
     inquirer
       .prompt({
 
+        type: "list" ,
+        message : "Hey! What would you like to do today?",
+        name:"choice",
+        choices:
+        [
+          "View All Employees?",
+          "View All Employees based on Roles?",
+          "View All Employees based on Department?",
+          "New Employee?",
+          "Update Employee?",
+          "Add New Department?",
+          "Add New Role?"
+        ]
+
       })
       .then(function(answer) {
+
+
+        switch(answer.choice){
+          case "View All Employees?":
+            employeeList();
+          break;
+          case "View All Employees based on Roles?":
+              viewByRoles();
+            break;
+          case "View All Employees based on Department?":
+              viewByDepartments();
+            break;
+          
+          case "New Employee?":
+                addEmployee();
+              break;
+
+          case "Update Employee?":
+                updateEmployee();
+              break;
+      
+            case "Add New Role?":
+                addRole();
+              break;
+      
+            case "Add New Department?":
+                addDepartment();
+              break;
+        }
  
       });
+  }
+
+  // View All Employee function
+
+  function employeeList(){
+    connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
+    function(err, res) {
+    if (err) throw err
+    console.table(res)
+    start()
+    })
   }
